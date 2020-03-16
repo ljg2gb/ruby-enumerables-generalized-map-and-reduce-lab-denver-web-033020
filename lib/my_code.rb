1fbe48f6a293cce08f264172e7692e6ce19fcc32
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-16 03:40:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,13 +10,16 @@ def map(source_array)
 end
 
 def reduce(array, starting_value = nil)
- if starting_value
-   num1 =sv 
-   i = 0 
-else
-  num1 = array[0]
-  i = 1 
-end
+  if starting_value
+    num1 =sv 
+    i = 0 
+  else
+    num1 = array[0]
+    i = 1 
+  end
+  
+  while i < starting_value.length 
+    yeild(num1, array[i])
 end
 
 map(source_array){ |n| n * n }
