cafe91af9a53335298e06a2ea2dc78e93d3f5adf
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-16 03:33:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,11 +1,12 @@
 # Your Code Here
 def map(source_array)
-  new_Array = []
+  new = []
   i = 0
   while i < source_array.length do
-  new_Array << yeild
+  new_Array << yeild(array[i])
   i +=1 
   end
+  new
 end
 
 map(source_array){ |n| n * n }
