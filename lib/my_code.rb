b44a932f295b39be8615ef32ca05cb33712cabbc
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-16 03:24:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
 # Your Code Here
+def map(source_array)
+  new_Array = []
+  i = 0
+  while i < source_array.length do
+  new_Array << yeild
+  i +=1 
+  end
+end
+
+map(source_array){ |n| n * n }
+map(source_array{ })
